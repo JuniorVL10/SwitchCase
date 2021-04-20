ee87a233f1d9882abfee301c7afc3b7226346e90
JuniorVL10 <joseluisvallejolazcano@gmail.com>
2021-04-19 20:16:32 -0700

    Agregar archivos de proyecto.

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //opciones 
+            Console.WriteLine("Elige una opcion\n " +
+                "\n1. - suma" +
+                "\n2. - resta" +
+                "\n3. - multiplicacon" +
+                "\n4. - division" +
+                "\n5. - salir"
+                );
+            string s1 = null;
+            s1 = Console.ReadLine();
+
+
+              switch (s1)
+                {
+                case "1":
+                    Console.WriteLine("Seleccionaste la suma");
+                        break;
+                case "2":
+                    Console.WriteLine("Seleccionaste la resta");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Seleccionaste la multiplicacion");
+                    break;
+
+                case "4":
+                    Console.WriteLine("Seleccionaste la division");
+                    break;
+
+                case "5":
+                    Console.WriteLine("Seleccionaste salir");
+                    break;
+                default:
+                    Console.WriteLine("La opcion es incorrecta");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
